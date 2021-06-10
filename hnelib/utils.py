80cce9f29b38a8cda59ce9fa8d535641b82d0214
hneutr <hunter.wapman@gmail.com>
2021-06-09 18:40:12 -0600

    adding empty directory

---
+++ hnelib/utils.py
@@ -0,0 +1,10 @@
+def empty_directory(directory, recursive=True, delete=True):
+    for f in directory.glob('*'):
+        if f.is_file():
+            f.unlink()
+        elif recursive:
+            empty_directory(f)
+            f.rmdir()
+
+    if delete and not len(list(directory.glob('*'))):
+        directory.rmdir()
