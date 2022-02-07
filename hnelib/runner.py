ba70dc9947c26f7807b09162f0f4d2da1e35f8c3
hneutr <hunter.wapman@gmail.com>
2022-02-07 11:27:47 -0700

    runner.clean bug

--- hnelib/runner.py
+++ hnelib/runner.py
@@ -469,6 +469,9 @@ class Runner(object):
                     collection_paths.add(base.joinpath(path))
 
         for base in bases:
+            if not base.exists():
+                continue
+
             for path in base.glob('**/*'):
                 stemless_path = path.parent.joinpath(path.stem)
 
