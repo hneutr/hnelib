1e02ee913eaaf4ffac1593c92ab32a28d7f2a135
hneutr <hunter.wapman@gmail.com>
2022-12-07 13:10:13 -0700

    20221207

--- hnelib/plt/arrow.py
+++ hnelib/plt/arrow.py
@@ -3,7 +3,7 @@ import functools
 from hnelib.plt.color import colors
 
 DEFAULT_ARROW = {
-    'linewidth': .5,
+    'linewidth': 1,
     'color': colors['-'],
     'arrowstyle': '->, head_width=.15, head_length=.21',
 }
