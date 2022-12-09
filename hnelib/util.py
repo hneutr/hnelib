e8d2f21032261bbd3897fc1cfa0bf3d27fc1748a
hneutr <hunter.wapman@gmail.com>
2022-12-08 18:32:42 -0700

    20221208

--- hnelib/util.py
+++ hnelib/util.py
@@ -1,3 +1,55 @@
+ONES_TO_WORD = {
+    '0': 'zero',
+    '1': 'one',
+    '2': 'two',
+    '3': 'three',
+    '4': 'four',
+    '5': 'five',
+    '6': 'six',
+    '7': 'seven',
+    '8': 'eight',
+    '9': 'nine',
+}
+
+TEENS_TO_WORD = {
+    '10': 'ten',
+    '11': 'eleven',
+    '12': 'twelve',
+    '13': 'thirteen',
+    '14': 'fourteen',
+    '15': 'fifteen',
+    '16': 'sixteen',
+    '17': 'seventeen',
+    '18': 'eighteen',
+    '19': 'nineteen',
+}
+
+IRREGULAR_TENS_TO_WORD = {
+    '20': 'twenty',
+    '30': 'thirty',
+    '40': 'forty',
+    '50': 'fifty',
+    '60': 'sixty',
+    '70': 'seventy',
+    '80': 'eighty',
+    '90': 'ninety',
+}
+
+NUMBER_TO_WORD = {
+    **ONES_TO_WORD,
+    **TEENS_TO_WORD,
+    **IRREGULAR_TENS_TO_WORD,
+}
+
+DIGIT_COUNT_TO_WORD = {
+    3: 'hundred',
+    4: 'thousand',
+    7: 'million',
+    10: 'trillion',
+    13: 'billion',
+}
+
+
 def empty_directory(directory, recursive=True, delete=True):
     for f in directory.glob('*'):
         if f.is_file():
@@ -94,3 +146,47 @@ def num_to_pretty_str(numbers):
         strings = [strings[0]]
 
     return strings
+
+
+def number_to_words(number, include_digit_count_words=True):
+    number_str = str(number)
+    decimal_str = ""
+
+    if '.' in number_str:
+        number_str, decimal_str = number_str.split('.')
+
+    parts = []
+    while number_str:
+        current_str = number_str[0]
+        next_str = number_str[1:]
+
+        if len(number_str) == 2:
+            if number_str[:2] in TEENS_TO_WORD:
+                current_str = number_str[:2]
+                next_str = number_str[2:]
+            elif current_str != '0':
+                current_str += '0'
+
+        current_word = NUMBER_TO_WORD[current_str]
+
+        if current_str != '0' or not parts:
+            parts.append(current_word)
+
+        if parts[-1] not in list(DIGIT_COUNT_TO_WORD.values()):
+            parts.append(DIGIT_COUNT_TO_WORD.get(len(number_str)))
+
+            if len(next_str) == 2:
+                parts.append("and")
+
+        number_str = next_str
+
+    parts = [p for p in parts if p]
+
+    if not include_digit_count_words:
+        last_part = parts[-1]
+        parts = [p for p in parts if p not in ["and"] + list(DIGIT_COUNT_TO_WORD.values())]
+
+        if last_part in list(DIGIT_COUNT_TO_WORD.values()):
+            parts.append(last_part)
+
+    return " ".join(parts)
