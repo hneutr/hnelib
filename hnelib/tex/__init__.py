f97ce50e10c889495db0c56c69d8dcac38190867
hneutr <hunter.wapman@gmail.com>
2023-06-01 10:35:46 -0600

    20230601

--- hnelib/tex/__init__.py
+++ hnelib/tex/__init__.py
@@ -2,10 +2,10 @@ import hnelib.tex.command
 import hnelib.tex.table
 
 def bold(string):
-    return f"\\bf{{{string}}}"
+    return f"{{\\bf {string}}}"
 
 def italic(string):
-    return f"\\it{{{string}}}"
+    return f"{{\\it {string}}}"
 
 def indent(string):
     return f"\\quad {string}"
