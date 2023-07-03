9ad169f27df542fa690c9be8fabc53978ee548c0
Hunter Wapman <hunter.wapman@gmail.com>
2023-07-03 17:34:19 -0400

    20230703

--- hnelib/plt/text.py
+++ hnelib/plt/text.py
@@ -1,4 +1,7 @@
+import matplotlib.pyplot as plt
 from matplotlib import transforms
+
+import hnelib.util
 
 from hnelib.plt.arrow import arrows
 from hnelib.plt.font import size as fontsize
@@ -80,50 +83,208 @@ def annotate(
             **kwargs,
         )
 
-def multiline(
-    ax,
-    lines,
-    x,
-    y,
-    va='bottom',
-    ha='left',
-):
-    1
+class Text(object):
+    def __init__(self, fig, ax):
+        self.fig = fig
+        self.ax = ax
+        self.inverse_transformer = self.ax.transData.inverted()
+        self.transform = self.ax.transData
 
-def multicolor(
-    ax,
-    elements,
-    x,
-    y,
-    va='bottom',
-    ha='left',
-    **kwargs,
-):
-    for i, element in enumerate(elements):
-        text, color = element if isinstance(element, tuple) else (element, 'black')
+    def draw(self, x, y, string, **kwargs):
+        kwargs.setdefault('bbox', {})
+        kwargs['bbox'] |= {
+            'pad': 0,
+            'fill': False,
+            'linewidth': 0,
+        }
 
-        if i != len(elements):
-            text += " "
-
-        text = ax.text(
+        self.text = self.ax.text(
             x,
             y,
-            text,
-            color=color,
-            transform=ax.transAxes,
-            va=va,
-            ha=ha,
+            string,
+            transform=self.transform,
             **kwargs,
         )
 
-        transforms.offset_copy(
-            text._transform,
-            x=text.get_window_extent().width,
-            units='dots',
+        self.text.draw(self.fig.canvas.get_renderer())
+
+    def get_coord(self, axis, bbox_point):
+        bbox = self.text.get_tightbbox(self.fig.canvas.get_renderer())
+
+        axes = ['x', 'y']
+        index = axes.index(axis)
+
+        coords = [0, 0]
+        coords[index] = getattr(bbox, bbox_point)
+
+        return self.inverse_transformer.transform(coords)[index]
+
+    def annotate(
+        self,
+        x,
+        y,
+        x_pad=0,
+        y_pad=0,
+        x_arrow_len=0,
+        y_arrow_len=0,
+        ha='center',
+        va='center',
+        clip_on=False,
+        arrowprops=arrows['->'],
+        fontsize=fontsize['annotation'],
+        bbox_kwargs={},
+        **kwargs,
+    ):
+        kwargs = {
+            'ha': ha,
+            'va': va,
+            'clip_on': clip_on,
+            'fontsize': fontsize,
+        } | kwargs
+
+        if ha == 'left':
+            x_mult = 1
+        elif ha == 'right':
+            x_mult = -1
+        else:
+            x_mult = 0
+            
+        if va == 'bottom':
+            y_mult = 1
+        elif va == 'top':
+            y_mult = -1
+        else:
+            y_mult = 0
+
+        x_start = x + (x_mult * x_pad)
+        y_start = y + (y_mult * y_pad)
+
+        x_end = x_start + (x_mult * x_arrow_len) 
+        y_end = y_start + (y_mult * y_arrow_len) 
+
+        self.draw(
+            x=x_end,
+            y=y_end,
+            string=self.string,
+            **kwargs,
         )
 
-    for s,c in zip(ls,lc):
-        text = plt.text(x,y,s+" ",color=c, transform=t, **kw)
-        text.draw(fig.canvas.get_renderer())
-        ex = text.get_window_extent()
-        t = transforms.offset_copy(text._transform, x=ex.width, units='dots')
+        if bbox_kwargs:
+            self.text.set_bbox(bbox_kwargs)
+
+        if x_start != x_end or y_start != y_end:
+            self.ax.annotate(
+                "",
+                (x_start, y_start),
+                xytext=(x_end, y_end),
+                arrowprops=arrowprops,
+                **kwargs,
+            )
+
+    def get_min_coord(self, axis):
+        return min([self.get_coord(axis=axis, bbox_point=f"{axis}{p}") for p in [0, 1]])
+
+    def get_max_coord(self, axis):
+        return max([self.get_coord(axis=axis, bbox_point=f"{axis}{p}") for p in [0, 1]])
+    
+
+class Substring(Text):
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self, raw, **kwargs):
+        super().__init__(**kwargs)
+        self.string, self.color = raw if isinstance(raw, tuple) or isinstance(raw, list) else (raw, self.DEFAULT_COLOR)
+
+    def draw(self, x, y, **kwargs):
+        return super().draw(
+            x,
+            y,
+            string=self.string,
+            color=self.color,
+            **kwargs,
+        )
+
+    def alter_string(self, pre="", post=""):
+        self.string = f"{pre}{self.string}{post}"
+    
+
+class Line(Text):
+    def __init__(self, substrings, line_number=0, n_lines=1, **kwargs):
+        super().__init__(**kwargs)
+        self.substrings = [Substring(substring, **kwargs) for substring in hnelib.util.as_list(substrings)]
+        self.line_number = line_number
+        self.n_lines = n_lines
+
+    @property
+    def string(self):
+        return "".join([ss.string for ss in self.substrings])
+
+    def alter_string(self, **kwargs):
+        for ss in self.substrings:
+            ss.alter_string(**kwargs)
+
+    def draw(self, x, y, **kwargs):
+        super().draw(
+            x,
+            y,
+            string=self.string,
+            alpha=0,
+            **kwargs,
+        )
+
+        x = self.get_min_coord(axis='x')
+
+        for key in ['ha', 'horizontalalignment']:
+            kwargs.pop(key, None)
+
+        kwargs['ha'] = 'left'
+
+        for substring in self.substrings:
+            substring.transform = self.transform
+            substring.draw(x, y, **kwargs)
+            self.transform = transforms.offset_copy(
+                substring.text._transform,
+                x=substring.text.get_window_extent().width * .95,
+                units='points',
+                fig=self.fig,
+            )
+
+
+class Paragraph(Text):
+    def __init__(self, lines, **kwargs):
+        super().__init__(**kwargs)
+        lines = hnelib.util.as_list(lines)
+        self.lines = [Line(l, line_number=i, n_lines=len(lines), **kwargs) for i, l in enumerate(lines)]
+
+    @property
+    def string(self):
+        return "\n".join([line.string for line in self.lines])
+
+    def draw(self, x, y, **kwargs):
+        kwargs.pop('string', None)
+
+        super().draw(
+            x,
+            y,
+            string=self.string,
+            alpha=0,
+            **kwargs,
+        )
+
+        y = self.get_min_coord(axis='y')
+
+        for key in ['va', 'verticalalignment']:
+            kwargs.pop(key, None)
+
+        kwargs['va'] = 'bottom'
+
+        for line in reversed(self.lines):
+            line.transform = self.transform
+            line.draw(x, y, **kwargs)
+            self.transform = transforms.offset_copy(
+                line.text._transform,
+                y=line.text.get_window_extent().height,
+                units='points',
+                fig=self.fig,
+            )
+            # y = line.get_max_coord(axis='y')
